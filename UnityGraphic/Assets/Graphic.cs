6bbe385be4855315559255d3b73bc571fc5eadbf
calvuS42 <olegka1302@gmail.com>
2018-03-18 12:36:24 +0200

    creating 10 cubes by prefab

---
+++ UnityGraphic/Assets/Graphic.cs
@@ -0,0 +1,23 @@
+﻿
+using UnityEngine;
+
+public class Graphic : MonoBehaviour {
+
+    public Transform pointPrefab;
+        
+
+	// Use this for initialization
+	void Start() {
+        for (int i = 0; i < 10; i++)
+        {
+            Transform point = Instantiate(pointPrefab);
+            point.localPosition = Vector3.right * i;
+        }
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
